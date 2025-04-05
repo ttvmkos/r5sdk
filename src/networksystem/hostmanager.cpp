cf06540cac86a4ec4bd74cf37753516d5cfcdde3
R5 Mkos <136424450+ttvmkos@users.noreply.github.com>
2025-04-05 07:52:42 -0400

    Update hostmanager.cpp

--- src/networksystem/hostmanager.cpp
+++ src/networksystem/hostmanager.cpp
@@ -19,6 +19,7 @@ CServerHostManager::CServerHostManager(void)
 {
 }
 
+
 //-----------------------------------------------------------------------------
 // Purpose: internal server launch handler
 //-----------------------------------------------------------------------------
@@ -29,8 +30,6 @@ static void HostManager_HandleCommandInternal(const char* const map, const char*
     Msg(eDLL_T::ENGINE, "Starting server with name: \"%s\" map: \"%s\" mode: \"%s\"\n",
         hostname->GetString(), map, mode);
 
-    bool hasPendingMap = *g_pPlaylistMapToLoad != '\0';
-
     // NOTE: when the provided playlist is the same as the one we're currently
     // on, and there's already a pending map load request,  the game will run 
     // "map <mapName>" in Playlists_Parse, where the map name is dictated by
@@ -38,16 +37,13 @@ static void HostManager_HandleCommandInternal(const char* const map, const char*
     // requested map here as to prevent Playlists_Parse from running the map
     // command on it, as we are going to run the changelevel command anyways.
     // Not doing this will result in running both map and changelevel commands.
-    if (changeLevel && hasPendingMap)
-    {
+    if (changeLevel)
         *g_pPlaylistMapToLoad = '\0';
-        hasPendingMap = false;
-    }
 
     const bool samePlaylist = v_Playlists_Parse(mode);
     char commandBuf[512];
 
-    if (!samePlaylist || !hasPendingMap)
+    if (!samePlaylist || !*g_pPlaylistMapToLoad)
     {
         snprintf(commandBuf, sizeof(commandBuf), "%s %s\n", changeLevel ? "changelevel" : "map", map);
         Cbuf_AddText(Cbuf_GetCurrentPlayer(), commandBuf, cmd_source_t::kCommandSrcCode);
